82bd1a232790510b8356af95352e074bb341d194
RGaldamez <ricardo.j.galdamez@gmail.com>
2020-05-20 19:03:56 -0600

    Adding autoscaling launch variables for instance setup

--- variables.tf
+++ variables.tf
@@ -10,4 +10,19 @@ variable "AVAILABILITY_ZONE_2" {
   default = "us-east-1b"
 }
 
+variable "AMI" {
+  default = "ami-085925f297f89fce1"
+}
+
+variable "INSTANCE_TYPE" {
+  default = "t2.micro"
+}
+
+variable "KEY_NAME" {
+  default  = "linux-connect"
+}
+
+
+
+
 
