627ca92aa3c89bddd09f15bb532037e7f75a5fc2
RGaldamez <ricardo.j.galdamez@gmail.com>
2020-05-20 20:08:16 -0600

    Adding application load balancer target group resource

--- application-load-balancer.tf
+++ application-load-balancer.tf
@@ -9,5 +9,32 @@ resource "aws_alb" "chat-application-load-balancer" {
     security_groups = [
         aws_security_group.chat-security-group.id
     ]
+}
+#autoscaling group will be listening in port 5000 so we want to forward HTTP traffic to its port
+resource "aws_alb_target_group" "chat-alb-target-group" {
+  name = "chat-alb-target-group"
+  port = "5000"
+  protocol = "HTTP"
+  vpc_id = aws_vpc.chat-vpc.id
+  tags = {
+      Name = "chat-alb-target-group"
+  }
+}
+
+#Listener will listen in port 80 and forward traffic to target group
+resource "aws_alb_listener" "chat-alb-listener" {
+    load_balancer_arn = aws_alb.chat-application-load-balancer.arn
+    port = 80
+    protocol "HTTP"
+
+    default_action {
+        target_group_arn = aws_alb_target_group.chat-alb-target-group.arn
+        type = "forward"
+    }
   
 }
+
+
+
+
+
