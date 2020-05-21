6fb975bbb54e0b771c317d291c618f3f281a039f
RGaldamez <ricardo.j.galdamez@gmail.com>
2020-05-20 18:38:22 -0600

    Creating 2 subnets and defining availability zones in variables

--- network.tf
+++ network.tf
@@ -10,3 +10,25 @@ resource "aws_vpc" "chat-vpc" {
     }
   
 }
+
+#Creating subnet in availability zone "a"
+resource "aws_subnet" "chat-subnet-1" {
+  vpc_id = aws_vpc.chat-vpc.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = var.AVAILABILITY_ZONE_1
+  tags = {
+      Name = "chat-subnet-a"
+  }
+}
+resource "aws_subnet" "chat-subnet-2" {
+  vpc_id = aws_vpc.chat-vpc.id
+  cidr_block = "0.0.2.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = var.AVAILABILITY_ZONE_2
+  tags = {
+      Name = "chat-subnet-2"
+  }
+}
+
+
--- variables.tf
+++ variables.tf
@@ -1,3 +1,13 @@
 variable "REGION" {
   default = "us-east-1"
 }
+
+variable "AVAILABILITY_ZONE_1" {
+  default = "us-east-1a"
+}
+
+variable "AVAILABILITY_ZONE_2" {
+  default = "us-east-1b"
+}
+
+
